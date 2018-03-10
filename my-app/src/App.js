4a5e0114bfbc5bf2898b6afa4b60d76397de77a8
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2018-03-10 16:35:54 -0500

    Linked Room-me firebase to App.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyBn3fdur2Tqfb1ZZMuhRWh9D6uk-b4fosg",
+	authDomain: "room-me-ee2e6.firebaseapp.com",
+	databaseURL: "https://room-me-ee2e6.firebaseio.com",
+	projectId: "room-me-ee2e6",
+	storageBucket: "room-me-ee2e6.appspot.com",
+	messagingSenderId: "912691567908"
+};
+firebase.initializeApp(config);
+var storage = firebase.storage();
+
 class App extends Component {
   render() {
     return (
