5a321a8c9f416a51157cd4c957931e39c21ef1f1
Isabelle del Mundo <isabelledelmundo@gmail.com>
2018-03-10 22:49:01 -0500

    yay got landing page to show up lmao

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LandingPage from './LandingPage';
 
-// Initialize Firebase
-var config = {
-	apiKey: "AIzaSyBn3fdur2Tqfb1ZZMuhRWh9D6uk-b4fosg",
-	authDomain: "room-me-ee2e6.firebaseapp.com",
-	databaseURL: "https://room-me-ee2e6.firebaseio.com",
-	projectId: "room-me-ee2e6",
-	storageBucket: "room-me-ee2e6.appspot.com",
-	messagingSenderId: "912691567908"
+const App = () => {
+  return (
+    <div className="App">
+      <LandingPage/>
+    </div>
+  );
 };
-firebase.initializeApp(config);
-var storage = firebase.storage();
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
 
 export default App;
+
+//Initialize Firebase
+// var config = {
+// 	apiKey: "AIzaSyBn3fdur2Tqfb1ZZMuhRWh9D6uk-b4fosg",
+// 	authDomain: "room-me-ee2e6.firebaseapp.com",
+// 	databaseURL: "https://room-me-ee2e6.firebaseio.com",
+// 	projectId: "room-me-ee2e6",
+// 	storageBucket: "room-me-ee2e6.appspot.com",
+// 	messagingSenderId: "912691567908"
+// };
+// firebase.initializeApp(config);
+// var storage = firebase.storage();
